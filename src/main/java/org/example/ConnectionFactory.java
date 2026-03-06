632a0f169fa2226b7b19558864282b979fc66995
MATHEUS FELIPE ALVES RIBEIRO <matheus_fa_ribeiro@estudante.sesisenai.org.br>
2026-03-06 15:59:00 -0300

    Feat: Criação da ConnectionFactory

---
+++ src/main/java/org/example/ConnectionFactory.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+    // Conexão com o Banco de Dados
+    public static Connection getConnection ()
+    {
+        String url = "jdbc:mysql://avnadmin:AVNS_3ucIBTLub3-ZmTK9ixO@socialmed2026-theuzinx23-d156.i.aivencloud.com:19307/defaultdb?ssl-mode=REQUIRED";
+        String user = "avnadmin";
+        String senha = "AVNS_3ucIBTLub3-ZmTK9ixO";
+
+        Connection conn = null;
+
+        try
+        {
+            return conn = DriverManager.getConnection(url, user, senha);
+        }
+        catch (SQLException e) {
+            System.err.println("Erro ao conectar ao banco");
+        }
+        return conn;
+    }
+}
