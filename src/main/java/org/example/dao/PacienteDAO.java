2160613d1ba4950c50848b9e974b4c5713d01718
MATHEUS FELIPE ALVES RIBEIRO <matheus_fa_ribeiro@estudante.sesisenai.org.br>
2026-03-06 20:35:57 -0300

    Teste

--- src/main/java/org/example/dao/PacienteDAO.java
+++ src/main/java/org/example/dao/PacienteDAO.java
@@ -1,4 +1,81 @@
 package org.example.dao;
 
-public class PacienteDAO {
+import org.example.configuracao.ConnectionFactory;
+import org.example.model.PacienteModel;
+
+import java.sql.*;
+
+public class PacienteDAO
+{
+    public boolean cadastrarPaciente(PacienteModel paciente) throws SQLException {
+        String sqlUsuario = "INSERT INTO Usuario (nome_Usuario, data_Nascimento, email_Usuario, telefone_Usuario, cpf_Usuario) VALUES (?, ?, ?, ?, ?)";
+        String sqlPaciente = "INSERT INTO Paciente (id_Usuario, endereco_Paciente) VALUES (?, ?)";
+
+        Connection conn = null;
+
+        try {
+            conn = ConnectionFactory.getConnection();
+            conn.setAutoCommit(false);
+
+            try (PreparedStatement stmt = conn.prepareStatement(sqlUsuario, Statement.RETURN_GENERATED_KEYS)) {
+                stmt.setString(1, paciente.getNomeUsuario());
+                stmt.setDate(2, Date.valueOf(paciente.getDataNascimentoUsuario()));
+                stmt.setString(3, paciente.getEmailUsuario());
+                stmt.setString(4, paciente.getSenhaUsuario());
+                stmt.setString(5, paciente.getTelefoneUsuario());
+                stmt.setString(6, paciente.getCpfUsuario());
+
+                stmt.executeUpdate();
+
+                try (ResultSet rs = stmt.getGeneratedKeys()) {
+                    if (rs.next()) {
+                        int idUsuarioGerado = rs.getInt(1);
+
+                        try (PreparedStatement stmtP = conn.prepareStatement(sqlPaciente)) {
+                            stmtP.setInt(1, idUsuarioGerado);
+                            stmtP.setString(2, paciente.getEnderecoPaciente());
+                            stmtP.executeUpdate();
+                        }
+
+                    } else {
+                        throw new SQLException("Falha a criar Usuario");
+                    }
+                }
+            }
+            conn.commit();
+            return true;
+
+        } catch (SQLException e)
+        {
+            if(conn != null)
+            {
+                try
+                {
+                    conn.rollback();
+                }
+                catch (SQLException ex)
+                {
+                    System.err.println("Erro ao dar rollback: " + ex.getMessage());
+                }
+            }
+            System.err.println("Erro ao cadastrar paciente: " + e.getMessage());
+            return false;
+        }
+        finally
+        {
+            if (conn != null)
+            {
+                try
+                {
+                    conn.setAutoCommit(true);
+                    conn.close();
+                }
+                catch (SQLException e)
+                {
+                    System.err.println("Erro ao fechar conexão: " + e.getMessage());
+                }
+            }
+        }
+    }
 }
+
--- src/main/java/org/example/dao/UsuarioDAO.java
+++ src/main/java/org/example/dao/UsuarioDAO.java
@@ -4,26 +4,25 @@ import org.example.configuracao.ConnectionFactory;
 import org.example.model.PacienteModel;
 import org.example.model.UsuarioModel;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class UsuarioDAO
 {
-    public UsuarioModel login (String cpf, String senha)
+    public UsuarioModel login(String cpf, String senha)
     {
         String sql = "SELECT * FROM Usuario WHERE cpf_usuario = ? AND senha_usuario = ?";
         UsuarioModel usuarioModel = null;
 
-        try(Connection conn = ConnectionFactory.getConnection();
-            PreparedStatement stmt = conn.prepareStatement(sql))
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql))
         {
             stmt.setString(1, cpf);
-            stmt.setString(2,senha);
+            stmt.setString(2, senha);
 
-            try(ResultSet rs = stmt.executeQuery()) {
-                if (rs.next()) {
+            try (ResultSet rs = stmt.executeQuery())
+            {
+                if (rs.next())
+                {
                     usuarioModel = new UsuarioModel(
                             rs.getInt("id_Usuario"),
                             rs.getString("nome_usuario"),
@@ -35,15 +34,11 @@ public class UsuarioDAO
                     );
                 }
             }
-        } catch (SQLException e) {
+        }
+        catch (SQLException e)
+        {
             throw new RuntimeException(e);
         }
         return usuarioModel;
-    }
-
-    public boolean cadastrarPaciente(PacienteModel paciente)
-    {
-        String sqlUsuario = "INSERT INTO Usuario (nome_Usuario, data_Nascimento, email_Usuario, telefone_Usuario, cpf_Usuario) VALUES (?, ?, ?, ?, ?)";
-        String sqlPacinte
     }
 }
