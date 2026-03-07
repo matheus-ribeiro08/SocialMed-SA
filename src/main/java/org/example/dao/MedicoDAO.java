c250bada807229ba764423ddafc8b21eb25a0345
MATHEUS FELIPE ALVES RIBEIRO <matheus_fa_ribeiro@estudante.sesisenai.org.br>
2026-03-06 21:34:41 -0300

    Feat: listarConsultaPorMedico na MedicoDAO

--- src/main/java/org/example/dao/MedicoDAO.java
+++ src/main/java/org/example/dao/MedicoDAO.java
@@ -1,4 +1,46 @@
 package org.example.dao;
 
-public class MedicoDAO {
+import org.example.configuracao.ConnectionFactory;
+import org.example.model.ConsultaModel;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.*;
+public class MedicoDAO
+{
+    public List<ConsultaModel> listarConsultasPorMedico(int idMedico) {
+        List<ConsultaModel> consultasMedico = new ArrayList<>();
+        String sql = "SELECT * FROM Consultas WHERE id_Medico = ? ORDER BY horario_Consulta ASC";
+
+        try (Connection conn = ConnectionFactory.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, idMedico);
+
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    ConsultaModel consulta = new ConsultaModel(
+                            rs.getInt("id_Consultas"),
+                            rs.getInt("id_Hospital"),
+                            rs.getString("local_Consulta"),
+                            rs.getInt("id_Paciente"),
+                            rs.getInt("id_Medico"),
+                            rs.getTimestamp("horario_consultas").toLocalDateTime()
+
+                    );
+                    consulta.setIdPaciente(rs.getInt("id_Paciente"));
+                    consulta.setIdMedico(rs.getInt("id_Medico"));
+
+                    consultasMedico.add(consulta);
+                }
+            }
+        } catch (SQLException e) {
+            System.err.println("Erro: " + e.getMessage());
+        }
+
+        return consultasMedico;
+    }
 }
--- src/main/java/org/example/model/ConsultaModel.java
+++ src/main/java/org/example/model/ConsultaModel.java
@@ -6,12 +6,11 @@ import java.time.LocalDateTime;
 public class ConsultaModel {
 
     private long idConsulta;
+    private int idHospital;
+    private String localEndereco;
+    private int idPaciente;
+    private int idMedico;
     private LocalDateTime horarioConsulta;
-
-    public ConsultaModel(long idConsulta, LocalDateTime horarioConsulta) {
-        this.idConsulta = idConsulta;
-        this.horarioConsulta = horarioConsulta;
-    }
 
     public long getIdConsulta() {
         return idConsulta;
@@ -21,6 +20,38 @@ public class ConsultaModel {
         this.idConsulta = idConsulta;
     }
 
+    public int getIdHospital() {
+        return idHospital;
+    }
+
+    public void setIdHospital(int idHospital) {
+        this.idHospital = idHospital;
+    }
+
+    public String getLocalEndereco() {
+        return localEndereco;
+    }
+
+    public void setLocalEndereco(String localEndereco) {
+        this.localEndereco = localEndereco;
+    }
+
+    public int getIdPaciente() {
+        return idPaciente;
+    }
+
+    public void setIdPaciente(int idPaciente) {
+        this.idPaciente = idPaciente;
+    }
+
+    public int getIdMedico() {
+        return idMedico;
+    }
+
+    public void setIdMedico(int idMedico) {
+        this.idMedico = idMedico;
+    }
+
     public LocalDateTime getHorarioConsulta() {
         return horarioConsulta;
     }
@@ -28,4 +59,15 @@ public class ConsultaModel {
     public void setHorarioConsulta(LocalDateTime horarioConsulta) {
         this.horarioConsulta = horarioConsulta;
     }
+
+    public ConsultaModel(long idConsulta, int idHospital, String localEndereco, int idPaciente, int idMedico, LocalDateTime horarioConsulta) {
+        this.idConsulta = idConsulta;
+        this.idHospital = idHospital;
+        this.localEndereco = localEndereco;
+        this.idPaciente = idPaciente;
+        this.idMedico = idMedico;
+        this.horarioConsulta = horarioConsulta;
+    }
+
+
 }
