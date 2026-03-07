5f942b0de0e57c509a9652c9ae3a8ae178b871a6
ADRIEL CAVALHEIRO <adriel_cavalheiro@estudante.sesisenai.org.br>
2026-03-06 21:36:03 -0300

    Fix

--- src/main/java/org/example/dao/MedicoDAO.java
+++ src/main/java/org/example/dao/MedicoDAO.java
@@ -1,4 +1,6 @@
 package org.example.dao;
 
 public class MedicoDAO {
+
+
 }
---
+++ src/main/java/org/example/view/IMedicoView.java
@@ -0,0 +1,11 @@
+package org.example.view;
+
+public interface IMedicoView {
+
+    int mostrarMenuMedico();
+
+
+
+
+
+}
