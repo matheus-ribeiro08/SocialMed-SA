b4ffdcbc8d0db71ec6ec72450730c8de2f3af247
THIAGO GONÇALVES DE LIS <thiago_lis@estudante.sesisenai.org.br>
2026-03-13 16:48:17 -0300

    Feat: Implementa as funcoes cadastrarMedico e cadastrarSecretario na AdminDAO

--- src/main/java/org/example/dao/AdminDAO.java
+++ src/main/java/org/example/dao/AdminDAO.java
@@ -1,4 +1,120 @@
 package org.example.dao;
 
+import org.example.database.ConnectionFactory;
+import org.example.model.MedicoModel;
+import org.example.model.SecretarioModel;
+
+import java.sql.*;
+
 public class AdminDAO {
+    public boolean cadastrarMedico(MedicoModel medico){
+        String sqlUsuario = "INSERT INTO Usuario (nome_usuario, data_Nascimento, email_Usuario, senha_Usuario, telefone_Usuario, cpf_Usuario) VALUES (?, ?, ?, ?, ?, ?)";
+        String sqlMedico = "INSERT INTO Medico (id_Usuario, especialidade_Medico) VALUES (?, ?)";
+
+        Connection conn = null;
+
+        try{
+            conn = ConnectionFactory.getConnection();
+            conn.setAutoCommit(false);
+
+            try(PreparedStatement stmtUsuario = conn.prepareStatement(sqlUsuario, Statement.RETURN_GENERATED_KEYS)){
+                stmtUsuario.setString(1, medico.getNomeUsuario());
+                stmtUsuario.setDate(2, Date.valueOf(medico.getDataNascimentoUsuario()));
+                stmtUsuario.setString(3, medico.getEmailUsuario());
+                stmtUsuario.setString(4, medico.getSenhaUsuario());
+                stmtUsuario.setString(5, medico.getTelefoneUsuario());
+                stmtUsuario.setString(6, medico.getCpfUsuario());
+
+                stmtUsuario.executeUpdate();
+
+                try(ResultSet rs = stmtUsuario.getGeneratedKeys()){
+                    if (rs.next()){
+                        int idUsuario = rs.getInt(1);
+
+                        try(PreparedStatement stmtMedico = conn.prepareStatement(sqlMedico)){
+                            stmtMedico.setInt(1, idUsuario);
+                            stmtMedico.setString(2, medico.getEspecialidadeMedico());
+
+                            stmtMedico.executeUpdate();
+                        }
+                    }else {
+                        throw new SQLException("Erro ao gerar ID do usuário");
+                    }
+                }
+            }
+
+            conn.commit();
+            return true;
+
+        } catch (SQLException e) {
+            if(conn != null){
+                try{
+                    conn.rollback();
+                } catch (SQLException ex){}
+            }
+
+            return false;
+        } finally {
+            if(conn != null){
+                try{
+                    conn.setAutoCommit(true);
+                    conn.close();
+                } catch (SQLException ex) {}
+            }
+        }
+    }
+
+    public boolean cadastrarSecretario(SecretarioModel secretario){
+        String sqlUsuario = "INSERT INTO Usuario (nome_usuario, data_Nascimento, email_Usuario, senha_Usuario, telefone_Usuario, cpf_Usuario) VALUES (?, ?, ?, ?, ?, ?)";
+        String sqlSecretario = "INSERT INTO Secretario (id_Usuario) VALUES (?)";
+
+        Connection conn = null;
+
+        try{
+            conn = ConnectionFactory.getConnection();
+            conn.setAutoCommit(false);
+
+            try(PreparedStatement stmtUsuario = conn.prepareStatement(sqlUsuario, Statement.RETURN_GENERATED_KEYS)){
+                stmtUsuario.setString(1, secretario.getNomeUsuario());
+                stmtUsuario.setDate(2, Date.valueOf(secretario.getDataNascimentoUsuario()));
+                stmtUsuario.setString(3, secretario.getEmailUsuario());
+                stmtUsuario.setString(4, secretario.getSenhaUsuario());
+                stmtUsuario.setString(5, secretario.getTelefoneUsuario());
+                stmtUsuario.setString(6, secretario.getCpfUsuario());
+
+                stmtUsuario.executeUpdate();
+
+                try(ResultSet rs = stmtUsuario.getGeneratedKeys()){
+                    if(rs.next()){
+                        int idUsuario = rs.getInt(1);
+
+                        try(PreparedStatement stmtSecretario = conn.prepareStatement(sqlSecretario)){
+                            stmtSecretario.setInt(1, idUsuario);
+                            stmtSecretario.executeUpdate()
+                        }
+                    } else{
+                        throw new SQLException("Erro ao gerar ID de usuário.")
+                    }
+                }
+            }
+            conn.commit();
+            return true;
+
+        } catch (SQLException e){
+            if(conn != null){
+                try{
+                    conn.rollback();
+                } catch (SQLException ex){}
+            }
+
+            return false;
+        }finally {
+            if(conn != null){
+                try{
+                    conn.setAutoCommit(true);
+                    conn.close();
+                }catch (SQLException ex) {}
+            }
+        }
+    }
 }
