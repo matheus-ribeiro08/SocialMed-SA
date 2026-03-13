676b17eedc99b8067639d915fa7586d1f1addf75
MIGUEL RICARDO SEBASTIANA <miguel_sebastiana@estudante.sesisenai.org.br>
2026-03-13 19:26:11 -0300

    Feat: Inicia roteador

--- src/main/java/org/example/roteador/RoteadorCadastro.java
+++ src/main/java/org/example/roteador/RoteadorCadastro.java
@@ -1,10 +1,19 @@
 package org.example.roteador;
 
+import org.example.presenter.CadastrarUsuarioPresenter;
 import org.example.view.CadastroViewConsole;
 
 public class RoteadorCadastro {
 
-    public void cadastroUser(){
-        CadastroViewConsole cadastroViewConsole = new CadastroViewConsole();
+    public void irPara(String destino){
+        switch (destino){
+            case "cadastro": {
+                CadastroViewConsole cadastroView = new CadastroViewConsole();
+                CadastrarUsuarioPresenter cadastroPresenter = new CadastrarUsuarioPresenter(this, cadastroView);
+
+            }
+
+
+        }
     }
 }
