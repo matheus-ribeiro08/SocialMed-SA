678fdb83e2229b59459dc9e12d78cceef5e145be
Miguel <miguelsebastiana44@gmail.com>
2026-03-22 12:09:27 -0300

    Feat: Adiciona atualizar e remover na secretarioDao

--- src/main/java/org/example/dao/SecretarioDAO.java
+++ src/main/java/org/example/dao/SecretarioDAO.java
@@ -121,4 +121,129 @@ public class SecretarioDAO {
         }
         return secretario;
     }
+
+    public boolean atualizarSecretario(SecretarioModel secretario) throws  SQLException {
+        String sqlUsuario = "UPDATE Usuario SET nome_Usuario = ?, email_Usuario = ?, senha_Usuario = ?, telefone_Usuario = ? WHERE id_Usuario = ?";
+        String sqlSecretario = "UPDATE Secretario SET turno_Secretario  = ? WHERE id_Secretario = ?";
+
+        Connection conn = null;
+
+
+        try {
+            conn = ConnectionFactory.getConnection();
+            conn.setAutoCommit(false);
+
+            try (PreparedStatement stmt = conn.prepareStatement(sqlUsuario)) {
+                stmt.setString(1, secretario.getNomeUsuario());
+                stmt.setString(2, secretario.getEmailUsuario());
+                stmt.setString(3, secretario.getSenhaUsuario());
+                stmt.setString(4, secretario.getTelefoneUsuario());
+                stmt.setInt(5, secretario.getIdUsuario());
+
+                int linhasAfetadas = stmt.executeUpdate();
+                if (linhasAfetadas == 0) {
+                    throw new SQLException("Usuario nao encontrado para atualização");
+                }
+
+            }
+
+            try (PreparedStatement stmt = conn.prepareStatement(sqlSecretario)) {
+                stmt.setString(1, secretario.getTurnoTrabalhadoSecretario());
+                stmt.setInt(2, secretario.getIdSecretario());
+
+                int linhasAfetadas = stmt.executeUpdate();
+                if (linhasAfetadas == 0) {
+                    throw new SQLException("Secretario nao encontrado para atualização");
+                }
+            }
+
+            conn.commit();
+            return true;
+
+        } catch (SQLException e) {
+            if (conn != null) {
+                try {
+                    conn.rollback();
+                } catch (SQLException ex) {
+                    System.err.println("Erro ao dar rollback!");
+                }
+            }
+            System.err.println("Erro ao atualizar secretario");
+            throw e;
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.setAutoCommit(true);
+                    conn.close();
+                } catch (SQLException e) {
+                    System.err.println("Erro ao fechar conexao");
+                }
+            }
+        }
+    }
+
+    public boolean removerSecretario(int idSecretario) throws SQLException{
+        String sqlBuscarIdUsuario = "SELECT id_Usuario FROM Secretario WHERE id_Secretario = ?";
+        String sqlRemoverSecretario = "DELETE FROM Secretario WHERE id_Secretario = ?";
+        String sqlRemoverUsuario = "DELETE FROM Usuario WHERE id_Usuario = ?";
+
+        Connection conn = null;
+        Integer idUsuario = null;
+
+        try {
+            conn = ConnectionFactory.getConnection();
+            conn.setAutoCommit(false);
+
+            try(PreparedStatement stmt = conn.prepareStatement(sqlBuscarIdUsuario)){
+                stmt.setInt(1 , idSecretario);
+                try(ResultSet rs = stmt.executeQuery()) {
+                    if(rs.next()){
+                        idUsuario = rs.getInt("id_Usuario");
+                    }else {
+                        throw new SQLException("Secretario nao encontrado");
+                    }
+
+                }
+            }
+
+            try(PreparedStatement stmt  = conn.prepareStatement(sqlRemoverSecretario)) {
+                stmt.setInt(1, idSecretario);
+                int linhasAfetadas = stmt.executeUpdate();
+                if(linhasAfetadas == 0){
+                    throw new SQLException("Erro ao remover secretario");
+                }
+            }
+
+            try(PreparedStatement stmt  = conn.prepareStatement(sqlRemoverUsuario)) {
+                stmt.setInt(1, idUsuario);
+                int linhasAfetadas = stmt.executeUpdate();
+                if(linhasAfetadas == 0){
+                    throw new SQLException("Erro ao remover usuario");
+                }
+            }
+
+            conn.commit();
+            return true;
+
+        } catch (SQLException e) {
+            if (conn != null) {
+                try {
+                    conn.rollback();
+                } catch (SQLException ex) {
+                    System.err.println("Erro ao dar rollback!");
+                }
+            }
+            System.err.println("Erro ao remover secretario");
+            throw e;
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.setAutoCommit(true);
+                    conn.close();
+                } catch (SQLException e) {
+                    System.err.println("Erro ao fechar conexao");
+                }
+            }
+        }
+    }
 }
