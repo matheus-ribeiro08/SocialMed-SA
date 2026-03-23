a790ec07d60820dc65ac3fa61fa5028feed5f887
Adriel Cavalheiro <adriel_cavalheiro@estudante.sesisenai.org.br>
2026-03-23 16:14:53 -0300

    FEAT: Adicona opÃpcao de cores em Ferramentas

--- src/main/java/org/example/utils/Ferramentas.java
+++ src/main/java/org/example/utils/Ferramentas.java
@@ -10,6 +10,29 @@ public class Ferramentas {
 
     }
 
+    public static final String RESET = "\u001B[0m";
+    public static final String BLACK = "\u001B[30m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String PURPLE = "\u001B[35m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String WHITE = "\u001B[37m";
+    public static final String GREENclaro ="\u001B[38;2;134;226;131m";
+    public static final String GREENescuro ="\u001B[38;2;0;128;0m";
+
+    // Fundos (backgrounds)
+    public static final String BG_BLACKBLUE ="\033[44m";
+    public static final String BG_BLACK = "\u001B[40m";
+    public static final String BG_RED = "\u001B[41m";
+    public static final String BG_GREEN = "\u001B[42m";
+    public static final String BG_YELLOW = "\u001B[43m";
+    public static final String BG_BLUE = "\u001B[44m";
+    public static final String BG_PURPLE = "\u001B[45m";
+    public static final String BG_CYAN = "\u001B[46m";
+    public static final String BG_WHITE = "\u001B[47m";
+
     public static long lLong(){
         long num = ler.nextLong();
 
